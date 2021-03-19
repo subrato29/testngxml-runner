a7d256957c21bd010616070582e53ae2cdd74516
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-03-19 14:40:59 -0400

    deleting unrequired imports

--- src/test/java/com/runner/testng/DynamicTestNg.java
+++ src/test/java/com/runner/testng/DynamicTestNg.java
@@ -2,11 +2,6 @@ package com.runner.testng;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
