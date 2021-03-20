52970c1c6d5df4ef88a953338b3b654b44fe5306
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-03-19 23:40:22 -0400

    code inlining

--- src/test/java/com/runner/testng/DynamicTestNg.java
+++ src/test/java/com/runner/testng/DynamicTestNg.java
@@ -10,35 +10,35 @@ import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
 
 public class DynamicTestNg {
-	static String testNgXml = "testng.xml";
-	public static void main(String[] args){
-		String testName = null;
-		try {
-			List<XmlClass> classList = new ArrayList<XmlClass>();
-	        XmlSuite suite = new XmlSuite();
-	        XmlTest test = new XmlTest(suite);
-	        
-			suite.setName("Suite");
-	        List<String> files = new ArrayList<String>();
-	        files.add(System.getProperty("user.dir")+File.separator+testNgXml);
-	        
-	        File file = new File(System.getProperty("user.dir") + File.separator 
-	    			+ "src" + File.separator + "test" + File.separator +"java"+ File.separator +"testscripts");
-	        String[] fileList = file.list();
-	        for(String name:fileList){
-	        	testName = name.split(".java")[0];
-	    		classList.add(new XmlClass("testscripts."+testName));
-		        test.setName("Test");
-		        test.setThreadCount(5);
-		        test.setXmlClasses(classList); 
-		        FileWriter writer = new FileWriter(new File(testNgXml));
-		        writer.write(suite.toXml());
-		        writer.flush();
-		        writer.close();
-	        }
-		}catch(Throwable t) {
-			t.printStackTrace();
-		}
-	}
+    static String testNgXml = "testng.xml";
+    public static void main(String[] args) {
+        String testName = null;
+        try {
+            List < XmlClass > classList = new ArrayList < XmlClass > ();
+            XmlSuite suite = new XmlSuite();
+            XmlTest test = new XmlTest(suite);
+
+            suite.setName("Suite");
+            List < String > files = new ArrayList < String > ();
+            files.add(System.getProperty("user.dir") + File.separator + testNgXml);
+
+            File file = new File(System.getProperty("user.dir") + File.separator +
+                "src" + File.separator + "test" + File.separator + "java" + File.separator + "testscripts");
+            String[] fileList = file.list();
+            for (String name: fileList) {
+                testName = name.split(".java")[0];
+                classList.add(new XmlClass("testscripts." + testName));
+                test.setName("Test");
+                test.setThreadCount(5);
+                test.setXmlClasses(classList);
+                FileWriter writer = new FileWriter(new File(testNgXml));
+                writer.write(suite.toXml());
+                writer.flush();
+                writer.close();
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
 
 }
